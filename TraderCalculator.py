b3cdf7a14b23dc0ff8edc24f4cef2e4eaf1a8e09
Salvador <40148006+Sirut@users.noreply.github.com>
2022-11-30 17:39:34 +0100

    Add files via upload

---
+++ TraderCalculator.py
@@ -0,0 +1,15 @@
+import webview
+
+class Api:
+    
+    def closePage(self):
+        window.destroy()
+     
+    def minimizePage(self):
+        window.minimize()
+
+if __name__ == '__main__':
+    api = Api()
+    
+    window = webview.create_window('', 'index.html', frameless=True, easy_drag=True, resizable=False, width=470, height=550, js_api=api)
+    webview.start(debug=False,http_server=True)
