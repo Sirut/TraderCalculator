d03ca855ac1bcb5ba27afb6f2233608481262702
Salvador <40148006+Sirut@users.noreply.github.com>
2022-12-01 12:18:54 +0100

    Update TraderCalculator.py

--- TraderCalculator.py
+++ TraderCalculator.py
@@ -11,5 +11,5 @@ class Api:
 if __name__ == '__main__':
     api = Api()
     
-    window = webview.create_window('', 'index.html', frameless=True, easy_drag=True, resizable=False, width=470, height=550, js_api=api)
-    webview.start(debug=False,http_server=True)
+    window = webview.create_window('Trader Calculator', 'index.html', frameless=True, easy_drag=True, resizable=False, width=470, height=550, js_api=api)
+    webview.start(debug=False,http_server=False)
